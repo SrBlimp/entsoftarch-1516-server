1374c899314f5f20cb84f09b4f2e155536cf8c23
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2016-02-25 16:57:12 +0100

    Changed name to CommentRepository

--- src/main/java/cat/udl/eps/entsoftarch/thesismarket/repository/CommentRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/thesismarket/repository/CommentRepository.java
@@ -4,10 +4,11 @@ package cat.udl.eps.entsoftarch.thesismarket.repository;
  * Created by Albert Merino
  */
 
+import cat.udl.eps.entsoftarch.thesismarket.domain.Comment;
 import cat.udl.eps.entsoftarch.thesismarket.domain.Proposal;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface CommentProposalRepository extends PagingAndSortingRepository<Proposal, Long> {
+public interface CommentRepository extends PagingAndSortingRepository<Comment, Long> {
 }
