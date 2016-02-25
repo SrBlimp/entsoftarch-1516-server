c7192c8765d88ed95716817a39a87767ebff16dd
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2016-02-25 17:16:56 +0100

    Optimized imports

--- src/main/java/cat/udl/eps/entsoftarch/thesismarket/repository/CommentRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/thesismarket/repository/CommentRepository.java
@@ -5,7 +5,6 @@ package cat.udl.eps.entsoftarch.thesismarket.repository;
  */
 
 import cat.udl.eps.entsoftarch.thesismarket.domain.Comment;
-import cat.udl.eps.entsoftarch.thesismarket.domain.Proposal;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
