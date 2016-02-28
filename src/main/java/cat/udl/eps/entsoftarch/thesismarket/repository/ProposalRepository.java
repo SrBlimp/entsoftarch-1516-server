d10eb529e5dddb5e5d3d69bee108b7913bfdfe60
Roberto García <roberto@rhizomik.net>
2016-02-28 19:38:36 +0100

    Add findByTitleContaining to ProposalRepository Add @Repository to repositories so they can be properly recognised by IntelliJ

--- src/main/java/cat/udl/eps/entsoftarch/thesismarket/repository/ProposalRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/thesismarket/repository/ProposalRepository.java
@@ -7,11 +7,17 @@ package cat.udl.eps.entsoftarch.thesismarket.repository;
 import cat.udl.eps.entsoftarch.thesismarket.domain.Proposal;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 @RepositoryRestResource
 public interface ProposalRepository extends PagingAndSortingRepository<Proposal, Long> {
 
     // PagingAndSortingRepository provides:
     // exists(ID id), delete(T entity), findAll(Pageable), findAll(Sort), findOne(ID id), save(T entity),...
-    // http://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/PagingAndSortingRepository.html
+    // http://docs.spring.io/spring-data/jpa/docs/current/reference/html/
+
+    List<Proposal> findByTitleContaining(String title);
 }
--- src/main/java/cat/udl/eps/entsoftarch/thesismarket/repository/ProposalSubmissionRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/thesismarket/repository/ProposalSubmissionRepository.java
@@ -4,7 +4,9 @@ package cat.udl.eps.entsoftarch.thesismarket.repository;
 import cat.udl.eps.entsoftarch.thesismarket.domain.ProposalSubmission;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @RepositoryRestResource
 public interface ProposalSubmissionRepository extends PagingAndSortingRepository<ProposalSubmission, Long> {
 }
