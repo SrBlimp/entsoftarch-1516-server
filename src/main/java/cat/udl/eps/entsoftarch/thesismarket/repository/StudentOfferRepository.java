10e2ff4466f53948f6fe084e8570f4f0632d8eea
Victor Pardos <victor.pardos1@gmail.com>
2016-03-01 13:04:44 +0100

    StudentOfferRepository add extends

--- src/main/java/cat/udl/eps/entsoftarch/thesismarket/repository/StudentOfferRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/thesismarket/repository/StudentOfferRepository.java
@@ -1,7 +1,10 @@
 package cat.udl.eps.entsoftarch.thesismarket.repository;
 
+import cat.udl.eps.entsoftarch.thesismarket.domain.Student;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
 /**
  * Created by victor on 29/02/16.
  */
-public interface StudentOfferRepository {
+public interface StudentOfferRepository extends PagingAndSortingRepository<Student,Long>{
 }
