739fe48bfa0c4e665d5cce3eafef2a9bc19b1d0b
Roberto García <roberto@rhizomik.net>
2016-03-03 15:09:07 +0100

    Add getters/setters for Event

--- src/main/java/cat/udl/eps/entsoftarch/thesismarket/domain/Event.java
+++ src/main/java/cat/udl/eps/entsoftarch/thesismarket/domain/Event.java
@@ -17,5 +17,11 @@ public abstract class Event {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     ZonedDateTime dateTime;
 
-    //User agent;
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public ZonedDateTime getDateTime() { return dateTime; }
+
+    public void setDateTime(ZonedDateTime dateTime) { this.dateTime = dateTime; }
 }
