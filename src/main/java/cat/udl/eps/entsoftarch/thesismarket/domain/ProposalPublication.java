8f68029a6f32e61c002e093ead24b6ac197f7599
Roberto García <roberto@rhizomik.net>
2016-03-05 23:37:48 +0100

    Add getters/setters to ProposalPublication

--- src/main/java/cat/udl/eps/entsoftarch/thesismarket/domain/ProposalPublication.java
+++ src/main/java/cat/udl/eps/entsoftarch/thesismarket/domain/ProposalPublication.java
@@ -1,13 +1,12 @@
 package cat.udl.eps.entsoftarch.thesismarket.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.annotation.Nullable;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -21,27 +20,49 @@ public class ProposalPublication extends Event {
     @NotNull
     private ProposalSubmission publishes;
     @OneToMany(mappedBy = "comments")
-    @JsonIgnore
     private Set<Comment> commentedBy = new HashSet<>();
     @OneToOne(mappedBy = "rejects")
-    @Nullable
-    @JsonIgnore
     private ProjectRejection rejectedBy;
     @OneToOne(mappedBy = "registers")
-    @Nullable
-    @JsonIgnore
     private ProposalRegistration registeredBy;
     @OneToMany(mappedBy = "target")
-    @JsonIgnore
     private Set<StudentOffer> interestedStudents = new HashSet<>();
     @OneToMany(mappedBy = "target")
-    @JsonIgnore
     private Set<DirectorOffer> interestedDirectors = new HashSet<>();
     @OneToMany(mappedBy = "target")
-    @JsonIgnore
     private Set<CoDirectorOffer> interestedCoDirectors = new HashSet<>();
 
-    public void setPublishes(ProposalSubmission publishes) {
-        this.publishes = publishes;
-    }
+    public Coordinator getAgent() { return agent; }
+
+    public void setAgent(Coordinator agent) { this.agent = agent; }
+
+    public ProposalSubmission getPublishes() { return publishes; }
+
+    public void setPublishes(ProposalSubmission publishes) { this.publishes = publishes; }
+
+    public Set<Comment> getCommentedBy() { return commentedBy; }
+
+    public void setCommentedBy(Set<Comment> commentedBy) { this.commentedBy = commentedBy; }
+
+    @Nullable
+    public ProjectRejection getRejectedBy() { return rejectedBy; }
+
+    public void setRejectedBy(@Nullable ProjectRejection rejectedBy) { this.rejectedBy = rejectedBy; }
+
+    @Nullable
+    public ProposalRegistration getRegisteredBy() { return registeredBy; }
+
+    public void setRegisteredBy(@Nullable ProposalRegistration registeredBy) { this.registeredBy = registeredBy; }
+
+    public Set<StudentOffer> getInterestedStudents() { return interestedStudents; }
+
+    public void setInterestedStudents(Set<StudentOffer> interestedStudents) { this.interestedStudents = interestedStudents; }
+
+    public Set<DirectorOffer> getInterestedDirectors() { return interestedDirectors; }
+
+    public void setInterestedDirectors(Set<DirectorOffer> interestedDirectors) { this.interestedDirectors = interestedDirectors; }
+
+    public Set<CoDirectorOffer> getInterestedCoDirectors() { return interestedCoDirectors; }
+
+    public void setInterestedCoDirectors(Set<CoDirectorOffer> interestedCoDirectors) { this.interestedCoDirectors = interestedCoDirectors; }
 }
