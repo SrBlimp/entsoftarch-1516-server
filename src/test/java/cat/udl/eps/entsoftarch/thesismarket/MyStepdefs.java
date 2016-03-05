461bc166d23fa1fa81feabcbfdd52d0f0fdddc0c
Roberto García <roberto@rhizomik.net>
2016-03-05 23:46:12 +0100

    Set @DirtiesContext in MyStepdefs so status reseted (data cleared) after each scenario

--- src/test/java/cat/udl/eps/entsoftarch/thesismarket/MyStepdefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/thesismarket/MyStepdefs.java
@@ -17,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationContextLoader;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -35,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Created by http://rhizomik.net/~roberto/
  */
 @ContextConfiguration(classes = {ThesismarketApiApplication.class}, loader = SpringApplicationContextLoader.class)
+@DirtiesContext
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 public class MyStepdefs {
