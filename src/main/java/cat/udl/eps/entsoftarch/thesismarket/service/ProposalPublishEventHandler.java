5c759de3b47b284f7352a5bfebb2a02a179496cf
Ferran Verdés <ferranverdes@gmail.com>
2016-03-07 16:55:02 +0100

    Added Scenario "Publish a proposal published "

---
+++ src/main/java/cat/udl/eps/entsoftarch/thesismarket/service/ProposalPublishEventHandler.java
@@ -0,0 +1,30 @@
+package cat.udl.eps.entsoftarch.thesismarket.service;
+
+import cat.udl.eps.entsoftarch.thesismarket.domain.Proposal;
+import cat.udl.eps.entsoftarch.thesismarket.domain.ProposalPublication;
+import cat.udl.eps.entsoftarch.thesismarket.domain.ProposalWithdrawal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+/**
+ * Created by ferran on 7/3/16.
+ */
+@Component
+@RepositoryEventHandler(ProposalPublication.class)
+public class ProposalPublishEventHandler {
+    final Logger logger = LoggerFactory.getLogger(ProposalWithdrawalEventHandler.class);
+
+    @HandleBeforeCreate
+    @Transactional
+    public void handleProposalPublicationPreCreated(ProposalPublication proposalPublication) {
+        logger.info("Before creating: {}", proposalPublication.toString());
+
+        Proposal proposal = proposalPublication.getPublishes().getSubmits();
+        Assert.isTrue(proposal.getStatus().equals(Proposal.Status.SUBMITTED), "The proposal is already published");
+    }
+}
--- src/test/resources/features/Publish proposal.feature
+++ src/test/resources/features/Publish proposal.feature
@@ -9,3 +9,9 @@ Feature: Publish proposal
     When I publish the proposal with title "Publish existing proposal submission"
     Then I have a proposal publication with title "Publish existing proposal submission"
 
+  Scenario: Publish a proposal published 
+    Given there is an existing proposal with title "Publish a proposal published" 
+    And there is an existing submission of the proposal titled "Publish a proposal published"
+    And there is an existing publication of the proposal titled "Publish a proposal published" 
+    When I publish the proposal with title "Publish a proposal published" 
+    Then I get error 500 with message "The proposal is already published"
