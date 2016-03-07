e88bfd2db672f72b96cea41bee80a421ac192433
alejandrocm94 <acm24@alumnes.udl.cat>
2016-03-07 16:02:48 +0100

    Added scenario

--- src/main/java/cat/udl/eps/entsoftarch/thesismarket/service/ProposalSubmissionEventHandler.java
+++ src/main/java/cat/udl/eps/entsoftarch/thesismarket/service/ProposalSubmissionEventHandler.java
@@ -3,20 +3,15 @@ package cat.udl.eps.entsoftarch.thesismarket.service;
 
 import cat.udl.eps.entsoftarch.thesismarket.domain.Proposal;
 import cat.udl.eps.entsoftarch.thesismarket.domain.ProposalSubmission;
-import cat.udl.eps.entsoftarch.thesismarket.domain.ProposalWithdrawal;
 import cat.udl.eps.entsoftarch.thesismarket.repository.ProposalRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.*;
-import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 import javax.transaction.Transactional;
 
-
-@Component
-@RepositoryEventHandler(ProposalSubmission.class)
 public class ProposalSubmissionEventHandler {
 
     final Logger logger = LoggerFactory.getLogger(ProposalSubmissionEventHandler.class);
@@ -27,17 +22,14 @@ public class ProposalSubmissionEventHandler {
     @HandleBeforeCreate
     @Transactional
     public void handleProposalSubmissionPreCreate(ProposalSubmission proposalSubmission){
-
         logger.info("Before creating: {}", proposalSubmission.toString());
 
-        Assert.notNull(proposalSubmission.getSubmits(), "Submits mustn't be null");
-        Proposal submits = proposalSubmission.getSubmits();
+       Proposal submits = proposalSubmission.getSubmits();
+        Assert.notNull(submits, "Trying to withdraw un-existing submission");
 
         Assert.isTrue(submits.getStatus().equals(Proposal.Status.DRAFT),
-                "Invalid proposal status '"+submits.getStatus()+"', should be '"+ Proposal.Status.DRAFT+"'");
+                "Invalid proposal status '"+submits.getStatus()+"', should be '"+ Proposal.Status.SUBMITTED+"'");
 
-        //proposalSubmission.setAgent(currentuser)
-        //Assert.isTrue(submits.getCreator().equals(submits.getCreator()));
 
         submits.setStatus(Proposal.Status.SUBMITTED);
         proposalRepository.save(submits);
