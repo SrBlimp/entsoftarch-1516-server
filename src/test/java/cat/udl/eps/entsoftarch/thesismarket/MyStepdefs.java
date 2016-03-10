fdf57bdff718b26bb67d334e0e6e39fd2353e675
Didac <didacflorensacazorla@gmail.com>
2016-03-10 15:24:55 +0100

    New scenario added - Trying edit proposal non-existent

--- src/test/java/cat/udl/eps/entsoftarch/thesismarket/MyStepdefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/thesismarket/MyStepdefs.java
@@ -11,6 +11,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
