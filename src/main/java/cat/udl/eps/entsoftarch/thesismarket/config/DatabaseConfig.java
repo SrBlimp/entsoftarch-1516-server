44026f9e9ac3c154e1c0060e16c41aa2b2ec7706
Roberto García <roberto@rhizomik.net>
2016-03-14 02:27:03 +0100

    Destasource config for Heroku profile

---
+++ src/main/java/cat/udl/eps/entsoftarch/thesismarket/config/DatabaseConfig.java
@@ -0,0 +1,24 @@
+package cat.udl.eps.entsoftarch.thesismarket.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by http://rhizomik.net/~roberto/
+ */
+@Configuration
+@Profile("heroku")
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
