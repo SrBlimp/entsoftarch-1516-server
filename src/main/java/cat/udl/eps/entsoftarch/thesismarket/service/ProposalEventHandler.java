5f5e52c8ea939cb8dd5cff149ba95cdd91972b8d
lobopetit <xavierbonell@gmail.com>
2016-05-25 15:31:43 +0200

    ProposalEventHandler try to improve

--- src/main/java/cat/udl/eps/entsoftarch/thesismarket/service/ProposalEventHandler.java
+++ src/main/java/cat/udl/eps/entsoftarch/thesismarket/service/ProposalEventHandler.java
@@ -10,21 +10,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.*;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 import javax.transaction.Transactional;
-import javax.validation.constraints.AssertFalse;
-import java.util.Arrays;
-import java.util.List;
 
 
 @Component
 @RepositoryEventHandler(Proposal.class)
 public class ProposalEventHandler {
 
-    final Logger logger = LoggerFactory.getLogger(ProposalWithdrawalEventHandler.class);
+    final Logger logger = LoggerFactory.getLogger(ProposalEventHandler.class);
 
     @Autowired private ProposalRepository proposalRepository;
     @Autowired private ProponentRepository proponentRepository;
@@ -45,6 +43,16 @@ public class ProposalEventHandler {
 
         proposalRepository.save(new_proposal);
 
+    }
+
+    @HandleBeforeCreate
+    @Transactional
+    @PreAuthorize("hasRole('PROPONENT')")
+    public void handleProposalBeforeCreate(Proposal proposal){
+        logger.info("Before creating: {}", proposal.toString());
+        //String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        //Proponent proponent = proponentRepository.findOne(username);
+        //proposal.setAgent(proponent);
     }
 
 }
