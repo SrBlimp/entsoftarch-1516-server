a047d5dfe2274bb46ba449702e78d7a81c25914b
Roberto García <roberto@rhizomik.net>
2016-06-01 16:40:33 +0200

    Add missing proposal publication dateTime to projection

--- src/main/java/cat/udl/eps/entsoftarch/thesismarket/domain/ProposalPublicationDetails.java
+++ src/main/java/cat/udl/eps/entsoftarch/thesismarket/domain/ProposalPublicationDetails.java
@@ -2,10 +2,13 @@ package cat.udl.eps.entsoftarch.thesismarket.domain;
 
 import org.springframework.data.rest.core.config.Projection;
 
+import java.time.ZonedDateTime;
+
 /**
  * Created by http://rhizomik.net/~roberto/
  */
 @Projection(name = "ProposalPublicationDetails", types = { ProposalPublication.class })
 public interface ProposalPublicationDetails {
     Proposal getProposal();
+    ZonedDateTime getDateTime();
 }
